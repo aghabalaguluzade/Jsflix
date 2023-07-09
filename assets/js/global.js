1ee3fe4f2ae6e31b5b97da40872eb0880f7611a0
aghabalaguluzade <agabala.oyunda@gmail.com>
2023-07-09 04:34:19 +0400

    css style end

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,8 +3,17 @@
 const searchBox = document.querySelector("[search-box]");
 const searchTogglers = document.querySelectorAll("[search-toggler]");
 
+const sideBar = document.querySelector("[sidebar]");
+const menuButton = document.querySelectorAll("[menu-btn]");
+
 searchTogglers.forEach(toggler => {
 	toggler.addEventListener("click", () => {
 		searchBox.classList.toggle("active");
+	});
+});
+
+menuButton.forEach(toggler => {
+	toggler.addEventListener("click", () => {
+		sideBar.classList.toggle("active");
 	});
 });
